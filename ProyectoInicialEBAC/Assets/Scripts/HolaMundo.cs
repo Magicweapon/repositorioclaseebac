1134113ae8e4bb5eb6ee90aabc37415f2ed949ff
Magicweapon <s_tar_craf_t@hotmail.com>
2022-07-01 19:40:43 -0500

    Hola Mundo
    - Se agregó un Script a la carpeta de Scripts

---
+++ ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola Mundo");
+        Debug.LogError("Algo salio muy mal");
+        Debug.LogWarning("Algo paso");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
