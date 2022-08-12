d80543e501a5229c789a2401b368e7bfee727727
Magicweapon <s_tar_craf_t@hotmail.com>
2022-08-12 18:39:52 -0500

    Uso del metodo Update
    Se utilizo el metodo Update

--- ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
+++ ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+    int x;
+    
     // Start is called before the first frame update
     void Start()
     {
+        x = 0;
+        
         Debug.Log("Hola Mundo");
         Debug.LogError("Algo salio muy mal");
         Debug.LogWarning("Algo paso");
@@ -15,6 +19,7 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        x = x + 1;
+        Debug.Log(x);
     }
 }
