677a444f77e7421c343f719d36079dd60ba6940a
Magicweapon <s_tar_craf_t@hotmail.com>
2022-09-23 18:26:06 -0500

    Escena y componentes
    - Se agregó una escena para guardar los cambios
    - Se crearon scripts para ejemplificar una condición de carrera

--- ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
+++ ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -5,21 +5,44 @@ using UnityEngine;
 public class HolaMundo : MonoBehaviour
 {
     int x;
-    
+
+    private void Awake()
+    {
+        Debug.Log("Hola desde Awake");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         x = 0;
         
-        Debug.Log("Hola Mundo");
-        Debug.LogError("Algo salio muy mal");
-        Debug.LogWarning("Algo paso");
+        Debug.Log("Hola Mundo desde Start");
+        //Debug.LogError("Algo salio muy mal");
+        //Debug.LogWarning("Algo paso");
     }
 
     // Update is called once per frame
     void Update()
     {
-        x = x + 1;
-        Debug.Log(x);
+        //x = x + 1;
+        //Debug.Log(x);
+
+        Debug.Log("Hola desde Update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde Fixed Update cada 50 frames");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde Late Update");
+    }
+    private void OnEnable()
+    {
+        Debug.LogWarning("El objeto ha sido habilitado");
+    }
+    private void OnDisable()
+    {
+        Debug.LogWarning("El objeto ha sido inhabilitado");
     }
 }
